273e18f2d3ba71b829ecad88d60d77fda8c407d1
Samuel Oliveira <samuelcontatodev@gmail.com>
2023-01-17 11:20:09 -0300

    configurando frontend

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import { Habit } from "./components/Habit";
+
+import './styles/global.css'
+
+export function App() {
+  return (
+    <div>
+      <Habit completed={5} />
+      <Habit completed={10} />
+      <Habit completed={20} />
+      <Habit completed={30} />
+    </div>
+  )
+}
