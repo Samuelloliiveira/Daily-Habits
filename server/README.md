a0b9512777e4c8e1d3e5235b783f8da5ecb18e6c
Samuel Oliveira <samuelcontatodev@gmail.com>
2023-01-18 10:41:36 -0300

    criando as primeiras rotas da aplicação, trabalhando com validação, e datas

---
+++ server/README.md
@@ -0,0 +1,5 @@
+// prisma-erd-generator -> Para gerar diagramas
+
+// httpie -> command line para operações HTTP
+
+// Seed -> Popula o BD com dados fictícios
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,15 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 /** 
- * se fosse para produção ia precisar
+ * CORS: se fosse para produção ia precisar
  * configurar quais rotas poderiam ter acesso ao backend
 */
 app.register(cors)
-
-app.get('/', async () => {
-
-    const habits = await prisma.habit.findMany({
-        // where: {
-        //     title: {
-        //         startsWith: 'Ler'
-        //     }
-        // }
-    })
-
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333,
