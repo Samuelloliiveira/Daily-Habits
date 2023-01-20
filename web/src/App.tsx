fab2b77c58f1dcf120a7d8610c885737e4a1af2c
Samuel Oliveira <samuelcontatodev@gmail.com>
2023-01-20 19:03:25 -0300

    Conectando backend, criando novos hábitos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,9 @@
-import { Header } from './components/Header';
-import { SummaryTable } from './components/SummaryTable';
+import './lib/dayjs'
 
 import './styles/global.css'
+
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 
 export function App() {
   return (
